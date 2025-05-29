5363d8145db9b43d86d21be219e306eb87be2aed
Hamiat Nalwanga <hamiat.nalwanga@flowfactory.com>
2025-05-29 18:36:48 +0200

    Refactored code and improved overall structure

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import datetime
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age_delta):
         self.name = name
-        self.age = age
+        self.age_delta = age_delta
 
 def validate_date(date_string) -> bool:
     try:
@@ -11,18 +11,16 @@ def validate_date(date_string) -> bool:
         return True
     except ValueError:
         return False
-    
-people = []
-equals_signs = "=" * 10
-dashes = "-" * 36  
 
-print(f"\n=== Welcome to my app!===\nAdd a name and a birthday for as many people as you want.\nWhen you are done, type 'x' to finish. I'll tell you who is the oldest and who is the youngest.\n\n{dashes}")
-
-def add_people():
+def add_people(dashes: str, people: list):
     while True:
         name = input("Please enter a name: ")
+        
+        if not name.strip():
+            print("❌  Name cannot be empty.\n")
+            continue
 
-        if name == "x" or name == "X":
+        if name.lower() == "x":
             print(f"Goodbye! Processing data...\n")
             break
         else:
@@ -37,20 +35,31 @@ def add_people():
             else:
                 print(f"❌  {name} was not added due to incorrect birthday format, should be yyyy-mm-dd.  \n\n{dashes}")
 
-add_people()
-print(f"{equals_signs} Result {equals_signs}")
+def main():
+    people = []
+    equals_signs = "=" * 10
+    dashes = "-" * 36
+    
+    print(f"\n=== Welcome to my app!===\nAdd a name and a birthday for as many people as you want.\nWhen you are done, type 'x' to finish. I'll tell you who is the oldest and who is the youngest.\n\n{dashes}")
+    
+    add_people(dashes, people)
+    print(f"{equals_signs} Result {equals_signs}")
 
-if len(people) > 1:
-    people_sorted = sorted(people, key=lambda person: person.age)
-    youngest = people_sorted[0]
-    oldest= people_sorted[-1]
-    print(f"The oldest is {oldest.name} and youngest is {youngest.name}")
-elif len(people) == 1:
-    print(f"Only one person ({people[0].name}) was added, so they are the oldest and youngest.")
-else:    
-    print("No data was found.")
+    if len(people) > 1:
+        people_sorted = sorted(people, key=lambda person: person.age)
+        youngest = people_sorted[0]
+        oldest= people_sorted[-1]
+        print(f"The oldest is {oldest.name} and youngest is {youngest.name}")
+    elif len(people) == 1:
+        print(f"Only one person ({people[0].name}) was added, so they are the oldest and youngest.")
+    else:    
+        print("No data was found.")
 
-print(f"{equals_signs * 2}========")
+    print(f"{equals_signs * 2}========")  
+
+    
+if __name__ == "__main__":
+    main()
 
 
 
